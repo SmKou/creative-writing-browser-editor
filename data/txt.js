eed1ffa580099337524b88af4a0ddbe79893a0a8
SmKou <66178856+SmKou@users.noreply.github.com>
2025-09-02 15:03:37 -0700

    Add data

---
+++ data/txt.js
@@ -0,0 +1,22 @@
+const chapter_2 = {
+	"8e30239d-e549-4482-a337-7a6cc2bd0480": `The city was fully illuminated by all kinds of creatures: ghost fire, witch fire, sprites, nature spirits, and the nature god’s moonlight. Much of the city was grid-like, separated into nine sectors distinguishable from above by rooftop colors, styles, and material, as well as wide boulevards spanning north-south and east-west from end to end. Rivers ran straight through the city, splitting into smaller rivers and narrow canals, some gathering at a lake in the northwestern sector. In the eastern sector, near to the city walls, there was a palace. Unlike much of the rest of the city, as if built atop a lake, the palace was illuminated by moonlight on the water, as well as oil lamps and chandeliers in its open corridors.`,
+	"c90fcc0a-225f-4a61-8513-731ecb70f518": `While there was plenty of activity in the ring of  outer sectors, the center was bustling and teeming with people. People were getting around by boat, cart, and beast, if they didn’t have wings, gliders, or spell-casted orbs, boards, and carpets. They crossed the rivers by bridge and by boat hopping. The air was filled with all the smells of food as if their smoke and steam formed its own atmosphere. Beneath it was a tumultuous sea of smells: perfumes, flowers, dirt roads and pavement, gardens, wet wood, unpolished wooden beams, paint, clay, stone, rotting flesh, blood, livestock and beastfolk, washed and unwashed bodies.`
+}
+
+const work_1 = {
+	title: "Lover in the Flesh",
+	draft_1: {
+		chapter_2: {
+			title: "Hot Spots",
+			order: [
+				"8e30239d-e549-4482-a337-7a6cc2bd0480",
+				"c90fcc0a-225f-4a61-8513-731ecb70f518"
+			]
+		}
+	}
+}
+
+export default {
+	work_1,
+	txt { chapter_2 }
+}
