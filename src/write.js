bb3341e65837d047ab7c010945403b162bf86078
SmKou <66178856+SmKou@users.noreply.github.com>
2025-09-04 15:28:43 -0700

    WIP Add write actions

---
+++ src/write.js
@@ -0,0 +1,41 @@
+const create = () => {}
+
+const select = () => {}
+
+const mselect = () => {}
+
+const deselect = () => {}
+
+const add = () => {}
+
+const insert = () => {}
+
+const rewrite = () => {}
+
+const copy = () => {}
+
+const move = () => {}
+
+const find = () => {}
+
+const find_all = () => {}
+
+const replace = () => {}
+
+const replace_all = () => {}
+
+export default {
+	new: create,
+	slt: select,
+	msl: mselect,
+	dsl: deselect,
+	add,
+	ins: insert,
+	rw: rewrite,
+	cp: copy,
+	mv: move,
+	find,
+	fa: find_all,
+	rp: replace,
+	ra: replace_all
+}
