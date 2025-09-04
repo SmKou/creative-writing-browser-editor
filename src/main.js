4a0bf7db78ba219eab56ef58a63abf08f6f3c4d9
SmKou <66178856+SmKou@users.noreply.github.com>
2025-09-03 21:22:09 -0700

    Add pointers

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
+import 'animate.css'
 import './style.css'
+
+document.addEventListener("click", () => document.getElementById("cmd-ln").focus())
+
+document.getElementById("cmd-ln").addEventListener("keydown", e => {
+	switch (e.key) {
+		case ".":
+		case "?":
+		case "!":
+			console.log("end sentence")
+			break;
+		case "Enter":
+			console.log("enter command or new segment")
+			break;
+	}
+})
+
+// <span id="end-here" class="animate__animated animate__flash">&#10566;</span>
+setTimeout(() => {
+	const span = document.createElement("span")
+	span.setAttribute("class", "animate__animated animate__flash")
+	span.id = "end-here"
+	span.innerHTML = "&#10566;"
+	document.getElementById("start-here").after(span)
+
+	setTimeout(() => {
+		span.remove()
+		document.querySelector("#start-here + span").after(span)
+	}, 1000)
+}, 3000)
