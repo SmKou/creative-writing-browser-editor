ccabd2ff940d044caf02cdd0b3407eaa99d6c419
SmKou <66178856+SmKou@users.noreply.github.com>
2025-09-08 22:25:44 -0700

    Add new sentence and back (simple)

--- src/main.js
+++ src/main.js
@@ -1,39 +1,36 @@
 // import editor from 'editor'
-import { default as test } from '../_storage/test/test_editor'
+import { default as test } from '../_storage/test/simple'
 import 'animate.css'
 import './style.css'
 
-const ipt = document.getElementById("cmd-ln")
+const handle_type = evt => {
+	if (evt.key == "Backspace" && !evt.target.value) {
+		evt.preventDefault()
+		test.back(evt.target)
+		evt.target.focus()
+	}
+	if (evt.key != "Enter")
+		return;
+	const user_input = evt.target.value.split("\n")[0]
+	const cmd = user_input.split(" ")[0]
+	if (!user_input) {} // new paragraph
+	else if (cmd[0] === "#") {}
+	else if (test.has(cmd)) {}
+	else {
+		test.create.sentence(evt.target, user_input)
+	} // new sentence
+	evt.target.focus()
+}
 
+const ipt = document.createElement("textarea")
+ipt.id = "ipt"
+ipt.addEventListener("keydown", handle_type)
+ipt.addEventListener("keyup", evt => {
+	if (evt.key == "Enter")
+		evt.target.value = ""
+	if (evt.key == "Backspace")
+		evt.preventDefault()
+})
 document.addEventListener("click", () => ipt.focus())
 
-ipt.addEventListener("keydown", e => {
-	if (e.key != "Enter")
-		return;
-	const user_input = ipt.value
-	if (!user_input) {
-		// todo: new paragraph
-	}
-
-	const cmd = user_input.split(" ")[0]
-	if (test.has(cmd)) {
-		// todo: execute command
-	}
-	else if (cmd[0] === "#") {
-		switch (cmd.length) {
-			case 1:
-				// todo: rename
-				break;
-			case 2:
-				// todo: new chapter
-				break;
-			case 3:
-				// todo: new section
-		}
-	}
-	else {
-		test.fn.create.sentence(user_input)(ipt, document.querySelector(".current"))
-	}
-})
-
-test.fn.load()
+test.load(ipt)
