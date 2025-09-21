7432129249328d6f6afefee6e70f41890bbcf40c
SmKou <66178856+SmKou@users.noreply.github.com>
2025-09-21 03:04:19 -0700

    Reorganize

--- src/ui.js
+++ src/ui.js
@@ -1,52 +1,8 @@
-/* UI Management
- * Case:
- * - create new sentence
- * - add txt to .current sentence and move .current to new sentence
- * - last.after(new sentence)
- *
- * Case:
- * - create new paragraph
- * - create new sentence
- * - add txt to .current sentence and move .current to new sentence
- * - new paragraph.apend(new sentence)
- * - last.after(new paragraph)
- *
- * Case: User press Enter after end mark (.|?|!)
- * - create new section (user input: title)
- * - create new paragraph
- * - remove .current&#ipt and new paragraph.append(.current&#ipt)
- * - (if no sections yet in chapter but were paragraphs: create prologue section)
- * - new section.append(new paragraph)
- * - last.after(new section)
- *
- * loc: d-c-n-p-s = segment[idx + 1]
- *
- * Case: User type command and input, press Enter
- * - Case: command contains "#"
- *   - #: new work
- *   - ##: new chapter
- *   - ###: new section
- * - Case: add <loc>
- *   default: last sentence of last paragraph (of last section)
- * - Case: ins <loc>
- *   default: first sentence of first paragraph (of first section)
- * - Case: mv <loc> -a|-i <loc>
- * - Case: rm <loc> -f <feature> <feature-keyword>
- * - Case: rn <[work|chapter|section]> <title>
- */
 import { temp_id } from "./write"
 
 const make = e => document.createElement(e)
 const text = e => document.createTextNode(e)
 const query = e => document.querySelector(e)
-
-const selected = {
-	draft: "",
-	chapter: "",
-	section: "",
-	paragraph: "",
-	sentence: ""
-}
 
 const create = {
 	sentence(id) {
@@ -113,7 +69,9 @@ const move = {
 		}
 	},
 	paragraph: {},
-	section: {}
+	section: {
+		has_pre_section: () =>
+	}
 }
 
 const name = {
@@ -128,18 +86,34 @@ const name = {
 	}
 }
 
-export const dom = {
-
-
-	end_triggered: (txt, ipt) => {
-		const new_sentence = sentence.create(temp_id())
-		const current = sentence.move.current(new_sentence, txt, ipt)
+export default {
+	end_trigger(sentence_id, txt, ipt) {
+		const new_sentence = create.sentence(sentence_id)
+		const current = move.sentence.current(new_sentence, txt, ipt)
 		current.after(new_sentence)
 		ipt.focus()
 	},
+	end_mark_enter(paragraph_id, sentence_id, txt, ipt) {
+		const new_paragraph = create.paragraph(paragraph_id)
+		const new_sentence = create.sentence(sentence_id)
+		const current = query(".paragraph:has(.current)")
+		move.sentence.current(new_sentence, txt, ipt)
+		new_paragraph.append(new_sentence)
+		current.after(new_paragraph)
+		ipt.focus()
+	},
+	enter(section_id, section_title, paragraph_id, sentence_id, ipt) {
+
+		const new_section = create.section(section_id, section_title)
+		const new_paragraph = create.paragraph(paragraph_id)
+		const current = query(".section:has(.current)")
+		sentence.move.ipt("paragraph", new_paragraph)
+		new_section.append(new_paragraph)
+	}
+
 	enter: {
 		w_input: (txt,ipt) => {
-			const current = query(".paragraph:has(.current)")
+			const current =
 			const new_paragraph = paragraph.create(temp_id())
 			const new_sentence = sentence.create(temp_id())
 			sentence.move.current(new_sentence, txt, ipt)
