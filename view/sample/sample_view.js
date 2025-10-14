82148ee27f3824a0f575cee3e3820cccdbc9540e
SmKou <66178856+SmKou@users.noreply.github.com>
2025-10-14 15:23:20 -0700

    Change to svelte

--- view/sample/sample_view.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("selectionchange", () => {
-	console.log(window.getSelection().toString())
-	const selected = document.getSelection().baseNode
-	console.log(selected.data, selected.parentElement.id);
-})
-
-// window.getSelection().toString(): highlighted portion
-// document.getSelection().baseNode.data: containing sen.
-// document.getSelection().baseNode.parentElement.id: location
