b3deb3461d65ac68b10f3b27b53b01475033185e
Stella Marie <66178856+SmKou@users.noreply.github.com>
2025-10-15 04:24:18 +0000

    WIP Add home and onload

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,23 @@
 <script>
+	import Work from './lib/Work.svelte'
+
+	const screen_orientation = {
+		main: "",
+		side: ""
+	}
+	// { id, title, feature, side }
+	const session = {}
+	const work = $state({
+		id: "",
+		title: ""
+	})
 </script>
 
 <header>
-	<h1></h1>
+    <h1>{ work.title }</h1>
 </header>
 <main class="main-none">
-	<article class="main">
+	
 </main>
 <footer>
 	<p><a href="https://github.com/SmKou/creative-writing-browser-editor">Creative Writing Browser editor</a> © 2025 by <a href="https://github.com/SmKou">Sm Kou</a> is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a><img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;"><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;"><img src="https://mirrors.creativecommons.org/presskit/icons/nc.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;"><img src="https://mirrors.creativecommons.org/presskit/icons/sa.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;"></p>
