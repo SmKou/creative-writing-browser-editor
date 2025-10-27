b2b858b518bdd1257ce484ba6334ff014a3d70b8
SmKou <66178856+SmKou@users.noreply.github.com>
2025-10-26 21:59:49 -0700

    WIP Add working data

--- src/storage.js
+++ src/storage.js
@@ -1,47 +1,346 @@
-const works = [
-	{
-		title: "Creative Writing Browser editor"
+const uuids = `
+a42a5604-2b49-422c-96b0-b03ad464bece
+
+def74162-2e68-48f5-9e98-bef99a216110
+
+7af54fc2-5cf9-4031-b8f2-62bdd001016d
+
+edb829c7-3381-4260-b401-b0ce7cd35627
+
+4672133a-2703-4b84-80b1-7eea39e56f41
+
+2d2ff845-380d-4dbf-a0d3-06298a162a5d
+
+42965035-dda2-4e83-8541-f1a6000dfa6a
+
+6e328a28-3a05-46c5-88d3-858aa765f928
+
+296a3dc3-f75d-4bcc-bacd-b80c22bc1cb9
+
+d2733bde-a242-414a-bf84-48d5ab13de27
+
+e8b6339c-e7b2-40b5-8ff6-6d0bdf634a63
+
+3c1418ff-f97d-4032-91e3-e0a76e791003
+
+fb87ab15-17c2-4352-b66b-d64837e9b877
+
+219208aa-9cbf-4b80-a309-ebc0325bb5ff
+
+e1aef49d-90cd-45d2-afdc-ab56a6f96c41
+
+0e5befb2-0c95-4d2d-a986-529d3285170a
+
+c28b1667-8933-4567-9ddd-39c7e719bfa3
+
+ead6e2f2-1434-424a-abc2-d3cfaaf974bc
+
+0675fd06-c8d8-4ebe-a3d1-7b66eee06aa8
+
+e25f5665-f329-401c-a742-a99146825614
+
+fa6f2ac4-363b-492f-bb1d-7503078ffdd9
+
+ec1e1a23-8f84-48db-b7d1-7f1f44aa192e
+
+aaa6b952-2f0d-4b6f-9443-9251fe9c7083
+
+e79b7a6c-12d6-4540-bd8b-ddd6c78b70c2
+
+b9afd0b9-472d-448d-94dd-f8b08271223f
+
+413d203c-e962-4c1b-b2f4-1c8da097c9ed
+
+898dd62f-5ed4-4731-9af1-3c84d983aafd
+
+361d3787-f245-42ef-a3e4-f31acb93b057
+
+7220ef5d-cc40-40ad-ab73-2a1fcad94739
+
+3bed01b1-c6b5-418c-897b-e020be4a3193
+
+686bf58c-1b79-4f33-8664-cc054cb6b4d0
+
+e3780df4-bb8f-41f1-9064-44484d2e83ba
+
+6488a80d-bff5-4807-a2a5-df9fcd286e19
+
+e327d24f-cd1c-4151-805d-afe657d650ec
+
+d3c7a039-869f-41fa-ab94-1698428ec6ba
+
+b2c7b224-81b1-4ef7-8575-9578071c9f05
+
+6b6f4d10-fd99-43bf-a25b-ac8281124ffb
+
+66ff04b0-6277-41e8-9867-84d53d310359
+
+ac01f8b4-92b3-48ad-ae1d-bff0087c3ff2
+
+764a7b5e-5717-4ad6-8085-e19d7a36cb52
+
+4d1fd069-94c1-4d6d-a15f-bfc6dbf4c4d5
+
+7e275c1e-c58e-4a37-97be-90fbb162f10c
+
+33212423-50cf-4fd9-9cf9-5e3e06cc8e18
+
+6f85e4b3-8ab1-47d3-a33d-4856ad0b0a13
+
+26f7d315-ec5a-441c-833f-e62e9b9e6744
+
+0d7ff16a-8f2b-410f-94b6-6d6a3549c6c1`
+
+const works = {
+	"f5fb79f6-10c7-46b5-a0db-7152f059d505": {
+		title: "Lover in the Flesh",
+		description: "Sumerine Mesotama descends from the eternal realm to assume her place in an inevitable, impending apocalypse in the kingdom of Isur. In the chaos of the Apocalypse, the Heavenly Emperor is freed from his prison and flees. While the deities of Isur panic, not knowing where he would go or what he would do, Sumerine awaits the arrival of her eternal enemy. Let the first round between creation and destruction commence.",
+		setting: [],
+		current: "",
+		drafts: [],
+		timelines: [],
+		profiles: [],
+		languages: [],
+		history: "",
+		journal: ""
+	},
+	"872800d6-60d9-44c3-9ae3-319145ea6c78": {
+		title: "Beyond the Gates",
+		description: "",
+		setting: ["Ijuum Hintk"],
+		drafts: [],
+		timelines: [],
+		profiles: [],
+		languages: [],
+		history: "",
+		journal: ""
+	},
+	"2fcb4fd9-fefd-4b33-be2e-d84aca851276": {
+		title: "Ghost City",
+		description: "",
+		setting: "Ijuum Hintk",
+		drafts: [],
+		timelines: [],
+		profiles: [],
+		languages: [],
+		history: "",
+		journal: ""
+	},
+	"bedd737a-400a-48b1-92cc-2e11592f3393": {
+		title: "",
+		description: "",
+		setting: "",
+		drafts: [],
+		timelines: [],
+		profiles: [],
+		languages: [],
+		history: "",
+		journal: "a57273e8-2070-4244-84b3-fc59ec7f9ab2"
 	}
-]
+}
 
-const journals = {}
+const settings = {
+	"bc5a249c-4049-4d16-b520-fbb649f8adea": {
+		name: "Ijuum Hintk",
+		scale: "",
+		description: "",
+		notes: [
+			{
+				txt: "",
+				links: []
+			}
+		]
+	},
+	"8a2ae54d-c3ee-4628-99db-aef53447b17e": {
+		name: "Ijuum Iduuh"
+	}
+}
 
-const drafts = {}
+const drafts = {
+	"86c380ed-b36e-4e5b-aef0-99324d8be75c": {
+		untitled: 1,
+		description: "",
+		current: {
+			chapter: "",
+			section: "",
+			paragraph: "",
+			sentence: ""
+		},
+		order: [],
+		outline: "",
+		history: ""
+	},
+	"c9426ab9-0b79-47cf-9d92-41e40a637335": {},
+	"c0330b59-1835-42c9-bb64-bfbfe1fbcd12": {},
+	"44ba1ea3-0ffd-4084-a898-61856e82c5f3": {},
+	"72f33d45-e118-4b74-b582-47bae52a50e8": {},
+	"743b1887-345f-43ad-ba29-2fc6a52d70c1": {},
+	"30bc4147-c7df-41fe-a479-90e97810c8fb": {}
+}
 
-const outlines = {}
+const chapters = {
+	"8d3a362e-20e7-470c-a890-fcc8fb3ccb2b": {
+		title: "",
+		description: "",
+		type: "",
+		order: [],
+		segments: {
+			"": {
+				title: "",
+				order: []
+			}
+		},
+		sentences: {
+			"": {
+				txt: "",
+				connections: {}
+			}
+		},
+		special: ""
+	}
+}
 
-const timelines = []
+const outlines = {
+	"f9519401-f8d9-4bf2-836b-af4dbd199b15": {
+		order: [],
+		segments: {
+			"": {
+				title: "",
+				description: "",
+				level: "",
+				txt: "",
+				todo: "",
+				connections: {}
+			}
+		},
+		reminders: {
+			todo: [],
+			datetime: {},
+			freq: {}
+		},
+		draft: ""
+	},
+	"9e8168d1-25ee-4752-905e-c22a75672277": {},
+	"9908fbd5-9b0d-4a7b-b8fc-47c349776fa1": {},
+	"a971a5d8-0100-4fd8-a8e6-126150800acf": {},
+	"0ee1cd52-caae-436c-8566-795f8d1bac47": {},
+	"0e83c0da-9ac9-4a76-8e46-8306c565ab36": {}
+}
 
-const profiles = []
+const timelines = {
+	"b8227ef7-eaf9-4f5c-9e9f-24f539c8c973": {
+		of: "PROFILE",	// Ra Lyri
+		order: [],
+		connections: {
+			"40ed93b0-e699-4137-bb47-0721be92e43a": "PROFILE"
+		},
+		events: {},
+		history: ""
+	},
+	"b7c68323-3f5d-457a-b2df-dd3c26ae8e8b": {},
+	"a0f02eaf-e772-4de4-82af-bacc922b074a": {},
+	"2dd25519-bf05-4791-b4ff-e988ee8aa325": {},
+	"19411c38-8af9-48a0-ba19-b97d9b0a205a": {},
+	"44f19504-ae21-496c-8e34-6aa03d2390ac": {},
+	"a99a4031-8771-4779-a50e-1f381a31b559": {},
+	"c3271316-0b85-414e-9965-07c682916b67": {},
+	"3f3c3e21-ed8c-4e9e-aea0-b5f957204174": {}
+}
 
-const lexicons = {}
+const profiles = {
+	"40ed93b0-e699-4137-bb47-0721be92e43a": {
+		name: "Ra Lyri",
+		type: "PERSON",
+		props: {},
+		relationships: {},
+		connections: {},
+		roles: [],
+		affiliations: [],
+		history: ""
+	},
+	"f5bc9a89-18b3-4b8d-8cd4-383f458da804": {
+		name: "Ni Nase"
+	},
+	"5d1480be-64b9-4c7a-afd3-917bfe9890dc": {
+		name: "Na Bali"
+	},
+	"481b085f-70e6-4bc6-a7b6-96f9253379f4": {
+		name: "Tari"
+	},
+	"e05c580a-bb25-41f2-80e4-52ef2cbc40a9": {
+		name: "Azura la Fonnta"
+	},
+	"48976611-324d-473c-9522-1499cb181e93": {
+		name: "Sumerine Mesotama"
+	},
+	"820dc0ee-8627-4e06-b4b6-abee5f43a159": {
+		name: "Shsyryo"
+	},
+	"a35d8353-9214-4be8-a76d-bd2b123f8055": {
+		name: "Suuriratke"
+	},
+	"9ced87ac-2c6f-4467-a710-1ddaff6c95c6": {
+		name: "Tahakat"
+	}
+}
 
-const glossaries = {}
+const languages = {
+	"601c68e7-caf3-4209-abf2-b29b8ca992ba": {
+		name: "",
+		setting: "",
+		connections: {},
+		lexicon: {
+			"": {
+				cat: "",
+				def: ""
+			}
+		},	// term: cat, def
+		glossary: {
+			"": {
+				def: "",
+				connections: {}
+			}
+		},	// term: def, connections (id:type)
+		grammar: {
+			rule: "",
+			explanation: "",
+			glossary: []
+		},
+		history: ""
+	},
+	"aa209612-20b5-4e6d-a8c0-8d0709e304a5": {},
+	"a082aed2-4006-4455-8603-3d5456cb857c": {}
+}
 
-const grammars = {}
+const journals = {
+	"a57273e8-2070-4244-84b3-fc59ec7f9ab2": {
+		work: "bedd737a-400a-48b1-92cc-2e11592f3393",
+		txt: [`The father wrote to persuade his son to pursue his chance in station and achieve aclaim, citing his own experiences as a warning to his son of his own short-sightedness. He too had once been infatuated with young love, torn from his current wife, but fate abandoned him and he took much longer to correct his path, and this is his warning to his son.
 
-const languages = []
+		The young woman the son's infatuated with and the father recognize themselves in one another, making his warning to his son a warning to her too. Do not follow the same path as him.
 
-const settings = []
+		Given their similar manners, the mother and father always compared their son to him, but their son's love of freedom and desire to love and be free was actually the mother's. Though promised since young, the father was enchanted by another young woman and the mother even helped him on his way down the wrong path. The mother found happiness and prestige as a singer of immense skill and her penchant for flamboyant spectacle, while the father found misfortune. Returning to her in disgrace, the mother felt a touch responsible for not stopping him (no matter that she had no sense of his impending misfortune). However, she refused to help him more than lodgings and fare unless he could prove that his skill was on par with hers. He worked exceptionally hard, studying, practicing, and despite repeated failure, entreated her to sing each new composition. It took a few years before his skill began to show and he could be introduced to the mother's crowd to prove he'd gone down the wrong path. In spite of his skill, he had little social capital and so, the mother married him, giving up her name and career, and helped him for years to reach the heights of his skill and prestige. In that time, as the memory of the mother was replaced in the social consciousness, they agreed to switch their personas, hence their children's eventual misunderstanding.`]
+	}
+}
 
-const histories = {}
+const histories = {
+	"": {
+		current: "",
+		order: [],
+		entity: "",
+		type: ""
+	}
+}
 
-export const current = {
-	work: works[0]
+const changes = {
+	"": {
+		action: "",
+		cond: {}
+	},	// cond: { args, implicit args }, src, from, to, res
 }
 
 export default {
-	works,
-	journals,
-	drafts,
-	outlines,
-	timelines,
-	profiles,
-	// maps,
-	lexicons,
-	glossaries,
-	grammars,
-	languages,
-	settings,
-	histories,
+	get_dashboard: () => {
+
+	}
 }
